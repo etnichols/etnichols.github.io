5be112db6e9bc819da10554ad74dc97081431a56
Evan Nichols <evantnichols@gmail.com>
2019-04-30 01:06:18 -0400

    add icon and update to use my .prettierrc

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,21 +1,21 @@
-import React, { FunctionComponent } from "react";
-import Layout from "../components/layout";
+import React, { FunctionComponent } from 'react'
+import Layout from '../components/layout'
 
-import { data, IDuration, IEntry, ISection } from "../data/resume";
-import styles from "../styles";
-import "./resume.css";
+import { data, IDuration, IEntry, ISection } from '../data/resume'
+import styles from '../styles'
+import './resume.css'
 
 const Resume: FunctionComponent<> = props => {
   return (
     <Layout>
       <RenderPage data={data} />
     </Layout>
-  );
-};
+  )
+}
 
 const RenderPage: FunctionComponent<{ data: [ISection] }> = ({ data }) => {
-  const col1 = data.slice(0, 1);
-  const col2 = data.slice(1);
+  const col1 = data.slice(0, 1)
+  const col2 = data.slice(1)
   return (
     <>
       <h1>resume.</h1>
@@ -24,8 +24,8 @@ const RenderPage: FunctionComponent<{ data: [ISection] }> = ({ data }) => {
         <Column sections={col2} />
       </div>
     </>
-  );
-};
+  )
+}
 
 /** Renders a column which can be multiple sections. */
 const Column: FunctionComponent<{ sections: [ISections] }> = ({ sections }) => {
@@ -39,12 +39,12 @@ const Column: FunctionComponent<{ sections: [ISections] }> = ({ sections }) => {
         />
       ))}
     </div>
-  );
-};
+  )
+}
 
 const Section: FunctionComponent<{ title: string; entries: [IEntry] }> = ({
   title,
-  entries
+  entries,
 }) => {
   return (
     <section>
@@ -54,24 +54,24 @@ const Section: FunctionComponent<{ title: string; entries: [IEntry] }> = ({
         <Entry key={`${title}-entry-${i}`} {...entry} />
       ))}
     </section>
-  );
-};
+  )
+}
 
 /**
  * A single entry, either a job entry or a list of skills. If the latter all
  * fields exception description are null.
  */
 const Entry: FunctionComponent<{
-  title?: string;
-  linkify?: string;
-  company?: string;
-  duration?: IDuration;
-  description: string | [string];
+  title?: string
+  linkify?: string
+  company?: string
+  duration?: IDuration
+  description: string | [string]
 }> = ({ title, linkify, company, duration, description }) => {
-  let header;
+  let header
 
   if (title && duration) {
-    const maybeLinkedTitle = linkify ? <a href={linkify}>{title}</a> : title;
+    const maybeLinkedTitle = linkify ? <a href={linkify}>{title}</a> : title
     header = (
       <>
         <h4 className="job-title">{maybeLinkedTitle}</h4>
@@ -84,7 +84,7 @@ const Entry: FunctionComponent<{
           <i>{`${duration.start} - ${duration.end}`}</i>
         </div>
       </>
-    );
+    )
   }
 
   const body = Array.isArray(description) ? (
@@ -95,14 +95,14 @@ const Entry: FunctionComponent<{
     </ul>
   ) : (
     description
-  );
+  )
 
   return (
     <div className="job">
       {header}
       <div className="description">{body}</div>
     </div>
-  );
-};
+  )
+}
 
-export default Resume;
+export default Resume
