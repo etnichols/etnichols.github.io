4e944d1f951bb97f757b2337dac51625272294d2
Evan Nichols <evantnichols@gmail.com>
2020-04-13 23:46:43 -0500

    finish new post list layout

--- src/components/postlist.js
+++ src/components/postlist.js
@@ -5,23 +5,36 @@ import { formatDate } from '../utils/format'
 import styles from '../styles'
 import './postlist.scss'
 
+const resolveColor = type => {
+  switch (type) {
+    case 'project':
+      return 'blue'
+    case 'tutorial':
+      return 'green'
+    default:
+      return 'red'
+  }
+}
+
 const PostList = ({ posts, withType }) => (
   <ul style={{ marginLeft: 0, listStyle: `none` }}>
     {posts.map(post => {
       const { node } = post
       const { fields, frontmatter } = node
       return (
-        <li key={fields.slug}>
+        <li className="post margin-bottom-medium" key={fields.slug}>
+          <div className="post-date">({formatDate(frontmatter.date)})</div>
           <div className="title-container">
-          {withType && (
-            <div className="post-type-container">
-            <div className="post-type">{frontmatter.type}</div>
-            </div>
-          )}
             <Link className="list-item" to={fields.slug}>
               {frontmatter.title}
             </Link>
-            <div className="post-date">({formatDate(frontmatter.date)})</div>
+            {withType && (
+              <div className="post-type-container">
+                <div className={`post-type ${resolveColor(frontmatter.type)}`}>
+                  {frontmatter.type}
+                </div>
+              </div>
+            )}
           </div>
         </li>
       )
