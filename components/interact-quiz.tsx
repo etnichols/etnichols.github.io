7e7d37840aacefae3264978ac493346151fceb8f
Evan Nichols <evan@tryinteract.com>
2025-10-07 12:12:52 -0400

    test embedding a quiz

---
+++ components/interact-quiz.tsx
@@ -0,0 +1,12 @@
+export default function InteractQuiz() {
+  return (
+    <iframe
+      id="interactApp68e3dfde6fc78d0252685cd6"
+      width="800"
+      height="800"
+      style={{ border: 'none', maxWidth: '100%', margin: 0 }}
+      allowTransparency={true}
+      src="https://quiz.tryinteract.com/#/68e3dfde6fc78d0252685cd6?method=iframe&mobile=1"
+    ></iframe>
+  )
+}
