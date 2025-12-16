6f224f9b97205791c956bdf9b170d2d693652774
Evan Nichols <evan@tryinteract.com>
2025-12-16 15:45:08 -0500

    fix up

--- app/embed-prod/page.tsx
+++ app/embed-prod/page.tsx
@@ -5,7 +5,17 @@ export default function EmbedProd() {
   return (
     <>
       <div id={`interact-${PROD_APP_ID}`}></div>
-      <Script>{`var ${PROD_APP_ID};(function(d, t){var s=d.createElement(t),options={"appId":"${PROD_APP_ID}","width":"800","height":"800","async":true,"host":"quiz.tryinteract.com","mobile":true};s.src='https://i.tryinteract.com/embed/app.js';s.onload=s.onreadystatechange=function(){var rs=this.readyState;if(rs)if(rs!='complete')if(rs!='loaded')return;try{${PROD_APP_ID}=new InteractApp();${PROD_APP_ID}.initialize(options);${PROD_APP_ID}.display();}catch(e){}};var scr=d.getElementsByTagName(t)[0],par=scr.parentNode;par.insertBefore(s,scr);})(document,'script');`}</Script>
+      <Script>{`
+      var ${PROD_APP_ID};(function(d, t){
+      var s=d.createElement(t),
+      options={"appId":"${PROD_APP_ID}","width":"800","height":"800","async":true,"host":"quiz.tryinteract.com","mobile":true};
+      s.src='https://i.tryinteract.com/embed/app.js';
+      s.onload=s.onreadystatechange=function(){
+        var rs=this.readyState;if(rs)if(rs!='complete')if(rs!='loaded')
+        return;
+      try{app_${PROD_APP_ID}=new InteractApp();
+      ${PROD_APP_ID}.initialize(options);app_${PROD_APP_ID}.display();}catch(e){}};
+      var scr=d.getElementsByTagName(t)[0],par=scr.parentNode;par.insertBefore(s,scr);})(document,'script');`}</Script>
     </>
   )
 }
